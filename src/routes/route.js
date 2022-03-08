1cb68173ae6a5c0523ede4ef5669ea24f1e2d97e
Manoj Koli <manojkoli.nmu@gmail.com>
2022-03-09 01:22:19 +0530

    JWT Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,29 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+
+
+let mid1=function(req,res,next){
+    let xAuthToken = req.headers["x-auth-token"]
+    if( xAuthToken != undefined){
+        console.log("done")
+        next()
+    }
+    else{
+        res.send("Please provide x-auth-token in header")
+    }
+}
+
+
 
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",mid1,userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",mid1,userController.updateUser)
+
+router.delete("/users/:userId",mid1,userController.deleteUser)
 
 module.exports = router;
